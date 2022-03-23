d0714278a36cdb2d0eeef91454726af0571eb7b0
han <han@smail.nju.edu.cn>
2022-03-23 10:22:04 +0800

    add select

--- server/server.py
+++ server/server.py
@@ -1,18 +1,27 @@
 import socket
+import selectors
 
 HOST="127.0.0.1"
 PORT=1234
+sel=selectors.DefaultSelector()
 
 #AF_INET means ipv4
 #SOCK_STREAM means TCP
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM)as s:
-    s.bind((HOST,PORT))
-    s.listen()
-    conn, addr=s.accept()
-    with conn:
-        print(f"Connected {addr}")
-        while True:
-            data=conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+print(f"Listening on {(HOST, PORT)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+try:
+    while True:
+        events=sel.select(timeout=None)
+        for key,mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key,mask)
+except KeyboardInterrupt:
+    print("Interrupt")
+finally:
+    sel.close()
